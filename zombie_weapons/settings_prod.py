0d6468bf4da4045cf97dac4f8675cdbbdee4a5d8
Jennifer Rullmann <jen@redmonocle.com>
2012-06-21 00:22:27 -0400

    fixed problem with template path, and linked to mysql db

--- zombie_weapons/settings_prod.py
+++ zombie_weapons/settings_prod.py
@@ -1,6 +1,17 @@
 import os
 
-current_path = os.getcwd()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'zombie_weapons',                      # Or path to database file if using sqlite3.
+        'USER': 'weapons',                      # Not used with sqlite3.
+        'PASSWORD': 'zombie_weapons',                  # Not used with sqlite3.
+        'HOST': 'ec2-72-44-61-78.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+current_path = '/srv/www/production/zombie_weapons'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
